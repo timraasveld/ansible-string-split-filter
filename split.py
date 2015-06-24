ae1cf07cc703d85f3d3c77eab76c83baec17a74d
Tom Paine <aioue@users.noreply.github.com>
2015-06-24 16:34:17 +0100

    Add standard Ansible exception handling

--- split.py
+++ split.py
@@ -1,10 +1,17 @@
+from ansible import errors
 import re
 
 def split_string(string, seperator=' '):
-    return string.split(seperator) 
+    try:
+        return string.split(seperator)
+    except Exception, e:
+        raise errors.AnsibleFilterError('split plugin error: %s, string=%s' % str(e),str(string) )
 
 def split_regex(string, seperator_pattern):
-    return re.split(seperator_pattern, string)
+    try:
+        return re.split(seperator_pattern, string)
+    except Exception, e:
+        raise errors.AnsibleFilterError('split plugin error: %s' % str(e))
 
 class FilterModule(object):
     ''' A filter to split a string into a list. '''
